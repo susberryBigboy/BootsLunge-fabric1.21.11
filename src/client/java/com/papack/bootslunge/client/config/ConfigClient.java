5e9a0012dc1c2102e583aac8f5a872f6018eeacc
papa <papackcraft@gmail.com>
2026-09-20 17:34:58 +0900

    1.0 実行時のy-velocityの制限 バニラジャンプによる影響を減らす +shiftで強力ブレーキ(yv=0)

--- src/client/java/com/papack/bootslunge/client/config/ConfigClient.java
+++ src/client/java/com/papack/bootslunge/client/config/ConfigClient.java
@@ -26,8 +26,7 @@ public class ConfigClient {
     public boolean directionalJump = true;
     public boolean quickDirectionalJump = true;
 
-    public int directionalJumpAngle = 60;
-
+    public int directionalJumpAngle = 45;
 
     public static ConfigClient load() {
         if (FILE.exists()) {
